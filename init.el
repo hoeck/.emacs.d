f420d12ae804e86f6415d32db8eadb110e4d6984
soehnel <soehnel@scionics.de>
2015-08-03 14:11:50 +0200

    Update melpa URL

--- init.el
+++ init.el
@@ -183,7 +183,7 @@
     (require 'package)
     (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                              ("marmalade" . "http://marmalade-repo.org/packages/")
-                             ("melpa" . "http://melpa.milkbox.net/packages/")))
+                             ("melpa" . "http://melpa.org/packages/")))
 
     ;; manually control package loading (at the end of this file)
     (setq package-enable-at-startup nil)
