ffe05b94bf7ed19bd89bb64ebb046de0caa6e04f
hoeck <eriksoehnel@gmail.com>
2016-06-15 22:08:11 +0200

    Scaling text like a browser

--- init.el
+++ init.el
@@ -123,6 +123,12 @@
 (require 'font-lock)
 (set-face-foreground 'font-lock-string-face "green4")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; scaling text like a browser
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-0") (lambda () (interactive) (text-scale-set 0)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; window enlargement & shrunkenizing
 
