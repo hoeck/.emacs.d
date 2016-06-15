3258aff2164f9a619327062413eca8ac5501d7c5
hoeck <eriksoehnel@gmail.com>
2016-06-15 22:09:41 +0200

    Keybinding to revert a buffer

--- init.el
+++ init.el
@@ -124,6 +124,23 @@
 (set-face-foreground 'font-lock-string-face "green4")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; make sure my own keydefs always take precedence
+;;; see http://stackoverflow.com/questions/683425/globally-override-key-binding-in-emacs#683575
+;;; example: (define-key my-keys-minor-mode-map (kbd "C-i") 'some-function)
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
+
+(my-keys-minor-mode 1)
+
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; scaling text like a browser
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
@@ -181,6 +198,15 @@
 (require 'transpose-frame)
 ;; see transpose-frame.el in .emacs.d
 ;; commands: flop-frame, flip-frame, transpose-frame
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; keybinding for revert buffer
+
+(defun revert-buffer-no-confirm ()
+  "Revert buffer without confirmation."
+  (interactive) (revert-buffer t t))
+
+(define-key my-keys-minor-mode-map (kbd "C-c C-v") 'revert-buffer-no-confirm)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; startup
